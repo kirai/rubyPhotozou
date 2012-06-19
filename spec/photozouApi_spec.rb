19af37a966b52b5856c0d88424ed56584ef62f96
Hector Garcia <hectorgarcia@gmail.com>
2012-06-19 12:01:49 +0900

    DRYing code

--- spec/photozouApi_spec.rb
+++ spec/photozouApi_spec.rb
@@ -9,8 +9,14 @@ describe "PhotozouApiClass" do
 
   it "should return album photos when calling photo_list" do
     q = { "type" => "album", "user_id" => USERID, "album_id" => '6712980', "limit" => 5 }
-    photosXml        = @photo.photo_list(q, 'photo_list_public')
+    photosXml        = @photo.photo_list_public(q)
     photosXml.should =~ /.*photo.*/ 
+  end
+
+  it "should return photo information when calling photo_info" do
+    q = { "photo_id" => 139780150 }
+    response = @photo.photo_info(q)
+    response.should =~ /.*photo_id.*/
   end
 
 end
