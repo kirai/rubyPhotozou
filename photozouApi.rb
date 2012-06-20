0b6aa850f9202d254889f8ef2be978823a03f49a
Hector Garcia <hectorgarcia@gmail.com>
2012-06-20 15:53:54 +0900

    Photo_list_public method and Helper method for hash to url params transformation

--- photozouApi.rb
+++ photozouApi.rb
@@ -2,7 +2,6 @@
 
 require 'rubygems'
 require 'open-uri'
-require 'rexml/document'
 require 'net/http'
 require 'base64'
 require 'pp'
@@ -14,11 +13,46 @@ AGENT = 'photozouapi.rb/ruby/#{RUBY_VERSION}'
 USERID = '2507715'
 API_URI_BASE = 'http://api.photozou.jp/rest/'
 
+class PhotozouHelper
+  def self.hashToHttpStr hash
+    return hash.map{ |k,v| "#{k.to_s}=#{v}"}.join("&")
+  end
+end
+
 class Photozou
   def self.user_info args
-    response = Nokogiri::XML open("http://api.photozou.jp/rest/user_info?user_id=#{args[:user_id]}")
+    begin
+      response = Nokogiri::XML open(API_URI_BASE + "user_info?user_id=#{args[:user_id]}", 
+                                    "User-Agent" => AGENT)
+    rescue
+       return false
+    end
+
     response.at_xpath("//rsp//info//user").children.inject({}) {|h, e| h[e.name.to_sym] = e.text if e.name != 'text'; h}
   end
+
+  def self.photo_info args
+     response = Nokogiri::XML open(API_URI_BASE + "photo_info?photo_id=#{args[:photo_id]}",
+                                   "User-Agent" => AGENT)
+     response.at_xpath("//rsp//info//photo").children.inject({}) {|h, e| h[e.name.to_sym] = e.text; h}
+  end
+
+  # Endpoint: http://api.photozou.jp/rest/photo_list_public  
+  # 概要: インターネットに公開されている写真の一覧を取得します
+  # 認証: 認証の必要はありません。
+  # HTTPメソッド: GET
+
+  def self.photo_list_public args
+     response = Nokogiri::XML open(API_URI_BASE + "photo_list_public?" + PhotozouHelper.hashToHttpStr(args),
+                                   "User-Agent" => AGENT)
+     photos = []
+     response.at_xpath("//rsp//info").children.each do |x|
+        photo = x.at_xpath("//photo").children.inject({}) {|h, e| h[e.name.to_sym] = e.text; h}
+        photos << photo
+     end
+     photos
+  end
+
 end
 
 def query_to_uri(hash)
--- spec/photozouApi_spec.rb
+++ spec/photozouApi_spec.rb
@@ -8,12 +8,12 @@ describe "PhotozouApiClass" do
     @photo = PhotozouB.new
   end
 
-  describe "User Info" do
+  describe "User Info GET" do
 
     before(:each) do
       @user_info = {
-        :user_id => '2386319',
-        :profile_url => 'http://photozou.jp/user/top/2386319',
+        :user_id => USERID,
+        :profile_url => 'http://photozou.jp/user/top/' + USERID,
         :nick_name => 'dd',
         :my_pic => 'http://photozou.jp/img/nophoto_70_mypic.gif',
         :photo_num => '11',
@@ -21,10 +21,47 @@ describe "PhotozouApiClass" do
       }
     end
 
-    it "returns a hash of user info data" do
-      user_info = Photozou.user_info( { :user_id => 2386319 } )
-      user_info.should == @user_info
+    it "returns a hash of user_info data" do
+      user_info = Photozou.user_info( { :user_id => USERID } )
+      user_info['user_id'].should == @user_info['user_id']
     end
+  end
+
+
+  describe "Photo Info GET" do
+     before(:each) do
+       @photo_info = {
+            :photo_id => '139780150',
+            :copyright => 'creativecommons'
+        }
+     end
+     
+     it "returns a hash with photo_info data" do 
+        photo_info = Photozou.photo_info( { :photo_id => 139780150 } )
+        photo_info['photo_id'].should == @photo_info['photo_id']
+        photo_info['copyright'].should == @photo_info['copyright']
+
+     end
+  end
+
+
+  describe "Photo List Public GET" do
+      before(:each) do
+        @photo_list = {
+          0 => {:photo_id => "139994143",
+                :album_id => "6712980" },
+          1 => {:photo_id => "139994143",
+                :album_id => "6712980" } 
+        }
+      end
+    
+      it "returns a hash with photo list data" do
+         args = { "type" => "album", "user_id" => USERID, "album_id" => '6712980', "limit" => 5 }
+         photo_list = Photozou.photo_list_public(args)
+         photo_list[0][:photo_id].should == @photo_list[0][:photo_id]
+         photo_list[1][:album_id].should == @photo_list[1][:album_id]
+         
+      end
   end
 
 
